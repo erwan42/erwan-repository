5ebc4f0c1eff6560a1267d2c369b252a1188c419
Erwan Mathieu <mathieue.erwan@gmail.com>
2012-09-17 16:07:07 +0200

    version 1.2

---
+++ Menu_eleve.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+?>
+<DOCTYPE html>
+<html>
+	<head>
+		<link href= "style01.css" rel="stylesheet" type "text/css" />
+		<title>Menu élève</title>
+	</head>
+
+<body>
+	<nav>
+		<h1>Menu<h1>
+		<a href="Acceuil.php">Accueil</a><br/>
+		<a href="Remplir dossier.php">Remplir dossier</a>
+	</nav>
